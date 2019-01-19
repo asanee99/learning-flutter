0d74588808d631f98448a1b0a3c87eb9bbaab4f0
ASANEE <asanee.tan@gmail.com>
2019-01-19 19:59:39 +0700

    assing some theme costomize

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,30 +1,24 @@
 import 'package:flutter/material.dart';
 
+import './products_manager.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.grey,
+        accentColor: Colors.deepPurple
+      ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: Column(
-            children: [
-              
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductsManager('Food Tester'),
+      ),
     );
   }
 }
--- flutter_course/lib/products_manager.dart
+++ flutter_course/lib/products_manager.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import './products.dart';
 
 class ProductsManager extends StatefulWidget {
+  final String startingProduct;
+  
+  ProductsManager(this.startingProduct);
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -8,20 +13,41 @@ class ProductsManager extends StatefulWidget {
   }
 }
 
+
 class _ProductManagerState extends State<ProductsManager> {
+  List<String> _products = [];
+
+  @override
+    void initState() {
+      _products.add(widget.startingProduct);
+      super.initState();
+    }
+
+  @override
+    void didUpdateWidget(ProductsManager oldWidget) {
+      // TODO: implement didUpdateWidget
+      super.didUpdateWidget(oldWidget);
+    }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: RaisedButton(
-        onPressed: () {
-          setState(() {
-            _products.add('Advanced Food Tester');
-          });
-        },
-        child: Text('Add Product'),
-      ),
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: RaisedButton(
+            color: Theme.of(context).primaryColor,
+            onPressed: () {
+              setState(() {
+                _products.add('Advanced Food Tester');
+              });
+            },
+            child: Text('Add Product'),
+          ),
+        ),
+        Products(_products)
+      ],
     );
   }
 }
